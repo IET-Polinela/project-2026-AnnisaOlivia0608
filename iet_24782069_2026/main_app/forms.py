0f5971f2cd21187b4c733b8f466d6d569ca907a7
AnnisaOlivia0608 <annisaolivia44@gmail.com>
2026-04-20 01:10:16 +0700

    LabSession 5, mengubah dan menambahkan kode untuk 6. Form Tambah & Edit

--- iet_24782069_2026/main_app/forms.py
+++ iet_24782069_2026/main_app/forms.py
@@ -4,4 +4,11 @@ from .models import Report
 class ReportForm(forms.ModelForm):
     class Meta:
         model = Report
-        fields = ['title', 'category', 'description', 'location']
+        fields = ['title', 'category', 'description', 'location', 'status']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'category': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'location': forms.TextInput(attrs={'class': 'form-control'}),
+            'status': forms.Select(attrs={'class': 'form-control'}),
+        }
