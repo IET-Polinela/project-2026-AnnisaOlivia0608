b0f66b1e049c333eb19fb2fe31e5f9628e9d7727
AnnisaOlivia0608 <annisaolivia44@gmail.com>
2026-04-29 14:44:43 +0700

    LabSession 7, 3. Implementasi Backend

--- iet_24782069_2026/dashboard_24782069/views.py
+++ iet_24782069_2026/dashboard_24782069/views.py
@@ -1,4 +1,21 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
+from django.http import JsonResponse
+from main_app.models import Report
+from django.db.models import Count
 
 class DashboardView(TemplateView):
     template_name = 'dashboard/index.html'
+
+class DashboardDataView(View):
+    def get(self, request):
+
+        # 🔹 Data status
+        status_data = Report.objects.values('status').annotate(total=Count('id'))
+
+        # 🔹 Data kategori
+        category_data = Report.objects.values('category').annotate(total=Count('id'))
+
+        return JsonResponse({
+            'status': list(status_data),
+            'category': list(category_data),
+        })
